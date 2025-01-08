dc947239484ee9048984427ad6f5a214d10bedd8
Kamil Andrzejewski <kamil.andrzejewski@stepstone.com>
2025-01-08 12:15:56 +0100

    feat: add swagger docs

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,26 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { serve, setup } from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
 import {
   createHomeRouter,
   createOrdersRouter,
   createProductsRouter,
 } from './routes';
+import { CommandBus, QueryBus } from './buses';
+import { connectDB } from './db';
 import { logger } from './utils';
 import { errorHandler, loggerMiddleware } from './middlewares';
-import { connectDB } from './db';
-import { CommandBus, QueryBus } from './buses';
 import {
+  CreateOrderCommandHandler,
   CreateProductCommandHandler,
   RestockProductCommandHandler,
   SellProductCommandHandler,
-  CreateOrderCommandHandler,
 } from './commands';
 import {
+  GetOrdersQueryHandler,
   GetProductByIdQueryHandler,
   GetProductsQueryHandler,
-  GetOrdersQueryHandler,
 } from './queries';
 
 const app = express();
@@ -39,10 +41,12 @@ queryBus.register('GetOrders', new GetOrdersQueryHandler());
 app.use('/', createHomeRouter());
 app.use('/api', createProductsRouter(commandBus, queryBus));
 app.use('/api', createOrdersRouter(commandBus, queryBus));
+app.use('/api-docs', serve, setup(swaggerDocument));
 
 app.use(errorHandler);
 
 const port = process.env.HTTP_PORT || 80;
+
 connectDB(process.env.MONGO_URI as string).then(() => {
   app.listen(port, () => {
     logger.info(`Server is running on port ${port}`);
--- src/db.ts
+++ src/db.ts
@@ -3,6 +3,7 @@ import { logger } from './utils';
 
 export const connectDB = async (uri: string) => {
   try {
+    logger.info('Start connecting to MongoDB');
     await mongoose.connect(uri);
     logger.info('MongoDB connected');
   } catch (error) {
